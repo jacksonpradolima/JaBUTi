1b7feb2b8ceff1a5d4e1519b1b1023e495328b50
glesio <glesio@17679ce6-aec3-481b-bd0a-90448c74a92c>
2012-06-14 19:49:20 +0000

    Remove print logs of debug

--- src/br/jabuti/project/TestCase.java
+++ src/br/jabuti/project/TestCase.java
@@ -162,19 +162,19 @@ public class TestCase {
 						// Adding new paths info...
 						for (int k = 0; k < pathList.length; k++) {
               
-              System.err.println("********************** pathList");
-              for (Object p : pathList[k]) {
-                System.err.println(p);
-              }
+			              /*System.err.println("********************** pathList");
+			              for (Object p : pathList[k]) {
+			                System.err.println(p);
+			              }*/
 
 
 							Object[] thePath = AbstractCriterion.changePath(
 							  cm.getCFG(), pathList[k]);
 
-              System.err.println("********************** thePath");
-              for (Object p : thePath) {
-                System.err.println(p);
-              }
+				              /*System.err.println("********************** thePath");
+				              for (Object p : thePath) {
+				                System.err.println(p);
+				              }*/
 														
 							for (int i = 0; i < Criterion.NUM_CRITERIA; i++) {								
 								criterion = cm.getCriterion(i);
